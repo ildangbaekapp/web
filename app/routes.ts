0ed6a2e37bf6f17f381fe21c7ff764594624c03d
parkcool <yickle1345@naver.com>
2025-07-10 09:54:41 +0000

    Feat: 회원가입 라우팅 추가

--- app/routes.ts
+++ app/routes.ts
@@ -8,6 +8,15 @@ import {
 export default [
   ...prefix("auth", [
     route("login", "./pages/auth/login.tsx"),
-    route("register", "./pages/auth/register.tsx"),
+    route(
+      "register",
+      "./components/Layout/RegisterLayout.tsx",
+      [
+        route("term", "./pages/auth/register/term.tsx"),
+        route("role", "./pages/auth/register/role.tsx"),
+        route("verify", "./pages/auth/register/verify.tsx"),
+        route("info", "./pages/auth/register/info.tsx"),
+      ]
+    ),
   ]),
 ] satisfies RouteConfig;
